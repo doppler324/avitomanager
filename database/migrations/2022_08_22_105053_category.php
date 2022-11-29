cf0f20923a8c06d337b90b5094eb0dc3261c7b1b
bauer <bauerd2010@gmail.com>
2022-11-29 11:09:24 +0700

    миграции добавление

---
+++ database/migrations/2022_08_22_105053_category.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('categories', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->string('name');
+      $table->string('avito_id');
+      $table->string('parent_category_id');
+      $table->integer('depth_level');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('categories');
+  }
+};
--- database/migrations/2022_11_29_035743_create_groups_projects_table.php
+++ database/migrations/2022_11_29_035743_create_groups_projects_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('groups_projects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('projects');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->timestamps();
+            $table->string('user_id');
         });
     }
 
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('groups_projects');
     }
 };
