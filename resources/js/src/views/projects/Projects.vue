444871c2c40a48cba9790ba8936a5d61b48df699
bauer <bauerd2010@gmail.com>
2022-11-30 23:51:40 +0700

    небольшие правки проектов

--- resources/js/src/views/projects/Projects.vue
+++ resources/js/src/views/projects/Projects.vue
@@ -168,8 +168,6 @@
               ></b-form-select>
             </b-form-group>
 
-            <b-button type="submit" variant="primary">Submit</b-button>
-            <b-button type="reset" variant="danger">Reset</b-button>
           </b-form>
         </b-modal>
       </div>
