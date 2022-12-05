bfa66f1f09432d45b456af414414a4183d3b6193
bauer <bauerd2010@gmail.com>
2022-12-05 23:35:16 +0700

    проекты

--- app/Http/Requests/categoryRequest.php
+++ app/Http/Requests/categoryRequest.php
@@ -23,8 +23,26 @@ class categoryRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
-        ];
+        switch ($this->method()) {
+            case 'GET':
+            case 'PUT':
+            case 'PATCH':
+            case 'DELETE':
+            {
+                return [
+                    'id' => 'required|exists:categories,id'
+                ];
+            }
+            case 'POST':
+            {
+                return [
+                    'name'=> 'required|min:3',
+                    'parent_category_id' => 'required|min:1|max:30',
+                    'depth_level' => 'required|min:1|max:10',
+                ];
+            }
+            default:
+                break;
+        }
     }
 }
